e96b6ad85c99d24234ab5c1636b94ae6c97fc7e9
rsutton <rsutton@8ac14d8a-0c34-0410-b91d-f940ec8b8c98>
2004-07-09 19:11:32 +0000

    submit form for adding a new link to the link database (code from ILN)

---
+++ trunk/html/links/submit.php
@@ -0,0 +1,47 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="../wwi.css">
+    <title>The Great War : Links : Submit a New Link</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <base href="http://reagan.library.emory.edu/rebecca/wwiweb/">
+  </head>
+<body>
+
+<?php
+// run everything as if one directory up
+chdir("..");
+include_once ("lib/alinkRecord.class.php");
+include_once("lib/mybreadcrumb.php");
+
+include("header.html");
+print "<p class='breadcrumbs'>" . $breadcrumb->show_breadcrumb() . "</p>";
+
+print '<div class="content">'; 
+
+$sort = $_GET["sort"]; // options: title|contrib|date
+$subject = $_GET['subj'];
+
+$args = array('host' => "vip.library.emory.edu",
+	      'db' => "WW1",
+	      'coll' => 'links',
+	      'debug' => false);
+$link = new aLinkRecord($args);
+
+print "<h2>Submit a new link</h2>";
+
+$link->printHTMLForm("add");
+
+print "</div>";
+
+print '<div class="sidebar">';
+include("searchbox.html");
+print "</div>";
+
+include("footer.html");
+
+
+?>
+
+</body>
+</html>
+

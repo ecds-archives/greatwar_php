cd53059ea4729a18776ceb90a8b25fe78758807c
rsutton <rsutton@8ac14d8a-0c34-0410-b91d-f940ec8b8c98>
2004-07-20 23:05:09 +0000

    header that uses basehref variable for menu links

---
+++ trunk/html/header.php
@@ -0,0 +1,24 @@
+<?php
+
+/* Note: the variable $basehref must be set before including this file */
+
+print "<p><img src='$basehref/images/topbanner.jpg' width='800' height='90'></p>
+
+ <table width='550' border='0' cellpadding='0' cellspacing='0' >
+     <tr>
+    	  <td><a href='$basehref/index.html'>Home</a></td>
+          <td>&#149;</td>
+	  <td><a href='$basehref/about.html'>About the Site</a></td>
+          <td>&#149;</td>
+	  <td><a href='$basehref/postcards/browse.php'>Postcards</a></td>
+          <td>&#149;</td>
+	  <td><a href='$basehref/poetry/'>Poetry</a></td>
+          <td>&#149;</td>
+	  <td><a href='$basehref/links/browse.php'>Links</a></td>
+       </tr>
+	  </table>
+
+<hr align='left'>
+";
+?>
+

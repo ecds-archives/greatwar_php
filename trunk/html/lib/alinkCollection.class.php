d47a2e2c2c9a4cfb2419c56fd4fbe3b42ee4c4e0
rsutton <rsutton@8ac14d8a-0c34-0410-b91d-f940ec8b8c98>
2004-07-20 22:45:42 +0000

    added functionality for approve - display approved status (yes/no) - allow administrator to approve links - sort by approval status

--- trunk/html/lib/alinkCollection.class.php
+++ trunk/html/lib/alinkCollection.class.php
@@ -8,15 +8,21 @@ class aLinkCollection extends linkCollection {
 
 
   function aLinkCollection ($argArray) {
-      parent::linkCollection($argArray);	   // call parent constructor first
-      // then override linkRecords with alinkRecords
-      $this->link = array();	// over-write as empty array
-      foreach ($this->ids as $i) {
-        $linkargs = $argArray;
-        $linkargs["id"] = $i;
-        $this->link[$i] = new alinkRecord($linkargs);
-        $this->link[$i]->taminoGetRecord();
-      }
+    $this->pretty_sort_opts['approve'] = "Approved";
+    $this->sortfield["approve"] = '@approved';
+
+    parent::linkCollection($argArray);	   // call parent constructor
+    // this array is defined in parent constructor overridden by 
+    array_push($this->sort_opts, "approve");
+
+    // then override linkRecords with alinkRecords
+    $this->link = array();	// over-write as empty array
+    foreach ($this->ids as $i) {
+      $linkargs = $argArray;
+      $linkargs["id"] = $i;
+      $this->link[$i] = new alinkRecord($linkargs);
+      $this->link[$i]->taminoGetRecord();
+    }
   }
 
   //print summary info ONLY if approved=yes
@@ -32,25 +38,28 @@ class aLinkCollection extends linkCollection {
   function printApprovalForm ($url) {
     $count = 0;
     print "<form action='$url'>";
-    print "<table>";
+    print "<table class='linkRecord'>";
+    print "<tr><th colspan='2'>Select the records you approve.</th></tr>";
+    print "<tr><th class='input'>Approve</th><th class='link'>Record summary</th></tr>";
     //foreach linkRecord that is not approved, print out a table row with checkbox
     foreach ($this->ids as $i) {
       if ($this->link[$i]->approved == "no") {
 	$count++;
-	print "<tr><td>";
+	print "<tr><td class='input'>";
 	print "<input type='checkbox' name='$i' checked='yes'></td>";
-	print "<td>";
+	print "<td class='link'>";
         $this->link[$i]->printSummary();
 	print "</td></tr>";
       }
     }
     if ($count == 0) {
       print "<tr><td></td><td>There are no link records to approve.</td></tr>";
-    } 
-    print "</table>";
-    if ($count != 0) {
-      print "<input type='submit' value='Approve Selected Links'></form>";
+    } else {
+      print "<tr><td colspan='2'><input type='submit' value='Approve Selected Links'></td></tr>";
     }
+    print "</table>";
+    print "</form>";
+
   }
 
 }

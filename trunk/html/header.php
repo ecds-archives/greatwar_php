b49c8c74fd6768ff4f20d762cbd33be086a34174
rsutton <rsutton@8ac14d8a-0c34-0410-b91d-f940ec8b8c98>
2004-08-11 20:43:53 +0000

    modified to use config.php variables

--- trunk/html/header.php
+++ trunk/html/header.php
@@ -1,20 +1,20 @@
 <?php
 
-/* Note: the variable $basehref must be set before including this file */
+/* Note: the variable $siteurl must be set (by config.php) before including this file */
 
-print "<p><img src='$basehref/images/topbanner.jpg' width='800' height='90'></p>
+print "<p><img src='$base_url/images/topbanner.jpg' width='800' height='90'></p>
 
  <table width='550' border='0' cellpadding='0' cellspacing='0' >
      <tr>
-    	  <td><a href='$basehref/index.html'>Home</a></td>
+    	  <td><a href='$base_url/index.html'>Home</a></td>
           <td>&#149;</td>
-	  <td><a href='$basehref/about.html'>About the Site</a></td>
+	  <td><a href='$base_url/about.html'>About the Site</a></td>
           <td>&#149;</td>
-	  <td><a href='$basehref/postcards/browse.php'>Postcards</a></td>
+	  <td><a href='$base_url/postcards/browse.php'>Postcards</a></td>
           <td>&#149;</td>
-	  <td><a href='$basehref/poetry/'>Poetry</a></td>
+	  <td><a href='$base_url/poetry/browse.php'>Poetry</a></td>
           <td>&#149;</td>
-	  <td><a href='$basehref/links/browse.php'>Links</a></td>
+	  <td><a href='$base_url/links/browse.php'>Links</a></td>
        </tr>
 	  </table>
 

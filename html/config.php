44c27dc5e228496cb43ba45ebc27901db7a5b476
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2004-08-11 20:53:27 +0000

    split out separate variable parts (hostname, base path for url, css file)

--- html/config.php
+++ html/config.php
@@ -4,13 +4,16 @@
 
 // root directory and url where the website resides
 $basedir = "/home/httpd/html/rebecca/wwiweb";
-$base_url = "http://reagan.library.emory.edu/rebecca/wwiweb/";
+$server = "reagan.library.emory.edu";
+$base_path = "/rebecca/wwiweb";
+$base_url = "http://$server/$base_path/";
 
 // add basedir to the php include path (for header/footer files and lib directory)
 set_include_path(get_include_path() . ":" . $basedir . ":" . "$basedir/lib");
 
 //shorthand for link to main css file
-$csslink = "<link rel='stylesheet' type='text/css' href='$base_url/wwi.css'>";
+$cssfile = "wwi.css";
+$csslink = "<link rel='stylesheet' type='text/css' href='$base_url/$cssfile'>";
 
 /* tamino settings common to all pages
    Note: all pages use same database, but there are three different collections

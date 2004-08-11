d9017e7fa74b969f7ea64c15bc71beb8e0447c92
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2004-08-11 20:48:34 +0000

    added csslink as a variable (rather than printing), added more common variables notably-- tamino database and server names

--- html/config.php
+++ html/config.php
@@ -1,13 +1,21 @@
 <?php
-/* Some minimal configuration for all wwi php pages */
 
-// root dir/url where the website is
+/* Configuration settings for entire site */
+
+// root directory and url where the website resides
 $basedir = "/home/httpd/html/rebecca/wwiweb";
-$basehref = "http://reagan.library.emory.edu/rebecca/wwiweb";
+$base_url = "http://reagan.library.emory.edu/rebecca/wwiweb/";
 
-print "<link rel='stylesheet' type='text/css' href='$basehref/wwi.css'>";
-
-// add basedir to the include path (for header/footer files and lib directory)
+// add basedir to the php include path (for header/footer files and lib directory)
 set_include_path(get_include_path() . ":" . $basedir . ":" . "$basedir/lib");
+
+//shorthand for link to main css file
+$csslink = "<link rel='stylesheet' type='text/css' href='$base_url/wwi.css'>";
+
+/* tamino settings common to all pages
+   Note: all pages use same database, but there are three different collections
+ */
+$tamino_server = "vip.library.emory.edu";
+$tamino_db = "WW1";
 
 ?>

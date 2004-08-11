384577113de36d0078c6ea872f64136b592866b3
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2004-08-11 20:52:46 +0000

    add more filename/display name mappings; adjusted to use config.php

--- html/lib/mybreadcrumb.php
+++ html/lib/mybreadcrumb.php
@@ -1,5 +1,5 @@
 <?php
-
+include("../config.php");
 include_once("lib/class.breadcrumb.inc.php");
 
 // set up a breadcrumb object to be used by all wwi pages
@@ -9,8 +9,17 @@ $breadcrumb->homepage = ''; //don't display webserver root as home
 $breadcrumb->dirformat='ucfirst'; // Show the directory in this style
 $breadcrumb->showfile=TRUE; // shows the file name in the path
 $breadcrumb->hideFileExt=TRUE;  // don't show filename extension
-// These mappings will change when the site moves to a more permanent home
-$breadcrumb->removeDirs= array('rebecca');
+
 $breadcrumb->changeName=array('wwiweb'=>'Home');
+$breadcrumb->changeFileName=array('$base_path/searchall.php'=>'Keyword Search',
+				  '$base_path/postcards/searchform.php'=>'Search',
+  				  '$base_path/postcards/search.php'=>'Search Results',
+  				  '$base_path/poetry/searchform.php'=>'Search',
+ 				  '$base_path/poetry/search.php'=>'Search Results',
+				  );
+/* TEMPORARY:
+  This mapping will change when the site moves to a more permanent home
+*/
+$breadcrumb->removeDirs= array('rebecca');
 
 ?>

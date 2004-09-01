d1b7c8bf03a959482570d632c8ff2cc9f5fea7c6
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2004-09-01 21:17:29 +0000

    added more named paths, edited directory links

--- html/lib/mybreadcrumb.php
+++ html/lib/mybreadcrumb.php
@@ -2,7 +2,7 @@
 
 /* Note: assumes that base_path is set, presumably by config.php */
 
-include_once("lib/class.breadcrumb.inc.php");
+include_once("lib/breadcrumb.class.php");
 
 // set up a breadcrumb object to be used by all wwi pages
 
@@ -14,12 +14,26 @@ $breadcrumb->hideFileExt = true;    // don't show filename extension
 
 $breadcrumb->changeName = array('wwiweb'=>'Home');
 $breadcrumb->changeFileName = array("$base_path/searchall.php" => "Keyword Search",
-				    "$base_path/postcards/searchform.php" => "Search",
+				    "$base_path/postcards/searchform.php" => "Advanced Search",
   				    "$base_path/postcards/search.php" => "Search Results",
-  				    "$base_path/poetry/searchform.php" => "Search",
+  				    "$base_path/poetry/searchform.php" => "Advanced Search",
  				    "$base_path/poetry/search.php" => "Search Results",
+     				    "$base_path/poetry/front.php" => "Front Matter",
       				    "$base_path/links/searchform.php" => "Search",
+       				    "$base_path/links/do_add.php" => "Process New Link",
+      				    "$base_path/admin/links/do_add.php" => "Process New Link",
+       				    "$base_path/admin/links/do_approve.php" => "Process Approval",
+       				    "$base_path/admin/links/do_modify.php" => "Process Modification",
+       				    "$base_path/admin/links/test.php" => "Test Links",
+       				    "$base_path/admin/links/do_modify.php" => "Process Modification",
 				  );
+
+$breadcrumb->changeDirLink = array("$base_path/postcards/" => "browse.php",
+				   "$base_path/poetry/" => "browse.php",
+				   "$base_path/links/" => "browse.php",
+				   "$base_path/admin/links/" => "view.php",
+				   );
+
 /* TEMPORARY:
   This mapping will change when the site moves to a more permanent home
 */

9e844f8302481807c2fffffb76735162c401a7a7
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2004-09-10 00:10:36 +0000

    added book title to document title & breadcrumbs

--- html/poetry/contents.php
+++ html/poetry/contents.php
@@ -1,23 +1,8 @@
 <?php
 include("../config.php");	
 
-print "<html>
-  <head>
-    $csslink
-    <title>The Great War : Poetry : Contents</title>
-    <meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>
-    <base href='$base_url'> 
-  </head> 
-<body>
-";
-
-/* Contents for a single volume of poetry */
-
 include_once("lib/taminoConnection.class.php");
 include_once("lib/mybreadcrumb.php");
-
-include("header.php");
-print "<p class='breadcrumbs'>" . $breadcrumb->show_breadcrumb() . "</p>";
 
 $id = $_GET['id'];
 
@@ -39,24 +24,48 @@ return <div>
 <teiHeader>
 <fileDesc> {$titlestmt}
 <sourceDesc>
-  <bibl>{$bibl}</bibl>
+  {$bibl}
 </sourceDesc>
 </fileDesc>
 </teiHeader>
-{ for $div1 in $a/:text//div1 
+{ for $fdiv in $a/:text/front/div1
+return <front><div1> {$fdiv/@id} {$fdiv/@n} {$fdiv/@type} {$fdiv/head} </div1></front> }
+{ for $div1 in $a/:text/body/div1 
    return <div1> {$div1/@id} {$div1/@n} {$div1/@type}
 {$div1/head} {$div1/p[1]}
     {for $div2 in $div1/div2 
       return <div2> {$div2/@id} {$div2/@n} {$div2/@type} 
            {$div2/docAuthor}
+        { for $div3 in $div2/div3 return <div3>{$div3/@id} {$div3/@n} {$div3/@type}</div3> }
         </div2> }
    </div1> }
 </div>';
 
 $xsl_file = "poetry.xsl";
 $xsl_params = array("mode" => "contents");
+$tamino->xquery($query, $pos, $maxdisplay);  
+$title = $tamino->findNode("title");
+$t = explode(":", $title, 2);
+$title = $t[0];
+$subtitle = $t[1];
 
-$tamino->xquery($query, $pos, $maxdisplay); 
+print "<html>
+  <head>
+    $csslink
+    <title>The Great War : Poetry : $title</title>
+    <meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>
+    <base href='$base_url'> 
+  </head> 
+<body>
+";
+
+/* Contents for a single volume of poetry */
+
+
+include("header.php");
+print "<p class='breadcrumbs'>" . $breadcrumb->show_breadcrumb();
+print " - <i>$title : $subtitle</i>";
+print "</p>";
 
 print '<div class="content">'; 
 $tamino->xslTransform($xsl_file, $xsl_params);

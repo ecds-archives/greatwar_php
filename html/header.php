265bbb8f542e4aff77fe0bdc9685d722c4e5f720
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2004-09-13 22:04:55 +0000

    commented out link to "about the site" (essay not yet available)

--- html/header.php
+++ html/header.php
@@ -7,9 +7,12 @@ print "<p><img src='$base_url/images/topbanner.jpg' width='800' height='90'></p>
  <table width='550' border='0' cellpadding='0' cellspacing='0' >
      <tr>
     	  <td><a href='${base_url}index.html'>Home</a></td>
-          <td>&#149;</td>
-	  <td><a href='${base_url}about.html'>About the Site</a></td>
-          <td>&#149;</td>
+          <td>&#149;</td>";
+
+// commented out until Rusche's essay is available
+//	  <td><a href='${base_url}about.html'>About the Site</a></td>
+print "  <td>About the Site</td>";
+print "   <td>&#149;</td>
 	  <td><a href='${base_url}postcards/index.php'>Postcards</a></td>
           <td>&#149;</td>
 	  <td><a href='${base_url}poetry/browse.php'>Poetry</a></td>

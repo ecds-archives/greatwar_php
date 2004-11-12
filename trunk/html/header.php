8b3f11e6a54e29b8eca7e2058735bc2f28a331de
rsutton <rsutton@8ac14d8a-0c34-0410-b91d-f940ec8b8c98>
2004-11-12 23:24:39 +0000

    display login info if set

--- trunk/html/header.php
+++ trunk/html/header.php
@@ -2,26 +2,35 @@
 
 /* Note: the variable $siteurl must be set (by config.php) before including this file */
 
-print "<p><img src='$base_url/images/topbanner.jpg' width='800' height='90'></p>
 
- <table width='550' border='0' cellpadding='0' cellspacing='0' >
+print "<p><img src='$base_url/images/topbanner.jpg' width='800' height='90' alt='The Great War 1914-1918'></p>";
+
+if (isset($_SESSION["name"])) {
+  // if logged in, display that fact
+  print "<p class='login'>\n";
+  print "Logged in as " . $_SESSION["name"];
+  print "<br><a href='${base_url}admin/logout.php'>Logout</a>\n";
+  print "</p>";
+
+}
+
+print "<table width='550' border='0' cellpadding='0' cellspacing='0' >
      <tr>
     	  <td><a href='${base_url}index.html'>Home</a></td>
-          <td>&#149;</td>";
+          <td>&bull;</td>";
 
 // commented out until Rusche's essay is available
 //	  <td><a href='${base_url}about.html'>About the Site</a></td>
 print "  <td>About the Site</td>";
-print "   <td>&#149;</td>
+print "   <td>&bull;</td>
 	  <td><a href='${base_url}postcards/index.php'>Postcards</a></td>
-          <td>&#149;</td>
+          <td>&bull;</td>
 	  <td><a href='${base_url}poetry/browse.php'>Poetry</a></td>
-          <td>&#149;</td>
+          <td>&bull;</td>
 	  <td><a href='${base_url}links/browse.php'>Links</a></td>
        </tr>
 	  </table>
-
-<hr align='left'>
+        <hr align='left'>
 ";
 ?>
 

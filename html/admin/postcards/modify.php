6a3ba09d04c9240de0284d381708bbb77030b20a
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2004-11-12 23:11:08 +0000

    working version of modify figure description; no formatting tags allowed

--- html/admin/postcards/modify.php
+++ html/admin/postcards/modify.php
@@ -19,10 +19,13 @@ include_once("lib/mybreadcrumb.php");
 include("header.php");
 print "<p class='breadcrumbs'>" . $breadcrumb->show_breadcrumb() . "</p>";
 
-$entity = $_GET["entity"]; 
+$entity = $_GET["id"]; 
+
 
 print '<div class="content"> 
           <h3>Modify Postcard Description</h3>';
+
+if (isset($entity)) {
 
 $args = array('host' => $tamino_server,
 	      'db' => $tamino_db,
@@ -31,13 +34,36 @@ $args = array('host' => $tamino_server,
 	      'imgpath' => 'http://chaucer.library.emory.edu/wwi/images/thumbnail/',
 	      'debug' => false);
 $desc = new figDesc($args);
-
 $desc->taminoGetRecord();
-
 $desc->printform("do_modify.php");
 
-print "</div>
-</body>
+/* NOTE: weird tamino/TEIform attribute problem still in play
+  ...  possible solution: allow limited html input, convert to tei & set
+  TEIform attributes */
+/* print '<hr><p><b>Allowed tags for formatting:</b><br>
+&lt;lb/&gt; : line break<br>
+&lt;hi rend="mode"&gt;text&lt;/hi&gt; where mode is one of: italic, bold, underline, smallcaps
+</p>';
+ print "<p><i>Note: for other formatting needs, please contact Rebecca.</i></p>";
+*/
+
+print '<hr><p><b>Allowed tags for formatting:</b><br> <i>Note: at this time,
+due to technical issues, no formatting tags are allowed.  Hopefully, this
+feature will be available soon.</i></p>';
+ 
+ 
+}  else {
+  print "<p>Error! No postcard was specified.</p>\n";
+}
+
+print "</div>";
+print '<div class="sidebar">'; 
+include("nav.html"); 
+include("searchbox.php"); 
+print '</div>'; 
+include("footer.html"); 
+
+print "</body>
 </html>";
 
 ?>

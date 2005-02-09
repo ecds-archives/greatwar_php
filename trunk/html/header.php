a7078f9ea7f4ecee5d0d04b3b4f9c9abd1eee47d
rsutton <rsutton@8ac14d8a-0c34-0410-b91d-f940ec8b8c98>
2005-02-09 00:38:07 +0000

    added links to 'about the site' essay & credits page

--- trunk/html/header.php
+++ trunk/html/header.php
@@ -14,20 +14,19 @@ if (isset($_SESSION["name"])) {
 
 }
 
-print "<table width='550' border='0' cellpadding='0' cellspacing='0' >
+print "<table width='700' border='0' cellpadding='0' cellspacing='0' >
      <tr>
     	  <td><a href='${base_url}index.html'>Home</a></td>
-          <td>&bull;</td>";
-
-// commented out until Rusche's essay is available
-//	  <td><a href='${base_url}about.html'>About the Site</a></td>
-print "  <td>About the Site</td>";
+          <td>&bull;</td>
+	  <td><a href='${base_url}about.php'>About the Site</a></td> ";
 print "   <td>&bull;</td>
 	  <td><a href='${base_url}postcards/index.php'>Postcards</a></td>
           <td>&bull;</td>
 	  <td><a href='${base_url}poetry/browse.php'>Poetry</a></td>
           <td>&bull;</td>
 	  <td><a href='${base_url}links/browse.php'>Links</a></td>
+          <td>&bull;</td>
+	  <td><a href='${base_url}credits.php'>Credits</a></td> 
        </tr>
 	  </table>
         <hr align='left'>

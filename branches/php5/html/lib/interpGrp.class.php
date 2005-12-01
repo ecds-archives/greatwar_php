9e95c31431861da1bed80744bd05c33be1fcef8a
rsutton <rsutton@8ac14d8a-0c34-0410-b91d-f940ec8b8c98>
2005-12-01 00:17:15 +0000

    use php5 xml functions to interpret xml

--- branches/php5/html/lib/interpGrp.class.php
+++ branches/php5/html/lib/interpGrp.class.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once("taminoConnection.class.php");
+include_once("xmlDbConnection.class.php");
 include_once("phpDOM/classes/include.php");
 
 class interpGrp {
@@ -19,7 +19,7 @@ class interpGrp {
   // Constructor 
   function interpGrp($argArray) {
     // pass host/db/collection settings to tamino object
-    $this->tamino = new taminoConnection($argArray);
+    $this->tamino = new xmlDbConnection($argArray);
 
     $this->groups = array();
     $this->groupnames = array();
@@ -42,19 +42,37 @@ class interpGrp {
 
   // retrieve interpGrps from tamino & initialize object values
   function taminoGetRecord() {
-    $rval = $this->tamino->xquery($this->xquery["getRecord"]);
+   $rval = $this->tamino->xquery($this->xquery["getRecord"]);
     if ($rval) {
-      $this->groups = array();
-      $this->names = array();
     } else {            // xquery succeeded
-      // should return all the interpGrps as branches
-      $xmlgroup = $this->tamino->xml->getBranches("ino:response/xq:result");
 
-      if ($xmlgroup) {
+      //convert xml tree to simple xml object
+      $sx = simplexml_import_dom($this->tamino->xml);
+
+      foreach ($sx->xpath("//interpGrp") as $g) {
+   	$grp = $g["type"];
+	  // add group name to list of interp groups
+	array_push($this->groupnames, $grp);
+	  // create an array for ids belonging to this group
+	$this->groups["$grp"] = array();
+	foreach ($g->interp as $i) {
+	  $id = $i["id"];
+	  $val = $i["value"];
+	    // add id to group array
+ 	  array_push($this->groups["$grp"], $id);
+	    // add index to pretty name array
+	  $this->names["$id"] = $val;
+	}
+      }
+
+      //      $xmlgroup = $this->tamino->xml->getBranches("ino:response/xq:result");
+      //      $children = $result->childNodes;
+
+      /*      if ($children) {
 	// Cycle through all of the branches (order doesn't matter)
-	foreach ($xmlgroup as $g) {
+	foreach ($children as $c) {
 	  //	  print "DEBUG: XMLString is" . $g->getXMLString() . "<br>\n";
- 	  $grp = $g->getTagAttribute("type");
+ 	  $grp = $c->getTagAttribute("type");
 	  // add group name to list
     	  array_push($this->groupnames, $grp);
 	  // create an array for ids belonging to this group
@@ -70,15 +88,15 @@ class interpGrp {
 	    $this->names[$id] = $val;
 	  } // foreach interp
 	} // foreach interpGrp
-      }
+      }  */
     }
   } 
 
   function printSummary () {
     foreach ($this->groupnames as $g) {
       print "$g<br>\n<ul>\n";
-      foreach ($this->groups[$g] as $i) {
-	print "<li>" . $this->names[$i] . "  ($i)</li>\n";
+      foreach ($this->groups["$g"] as $i) {
+	print "<li>" . $this->names["$i"] . "  ($i)</li>\n";
       }
       print "</ul>\n";
     }
@@ -86,14 +104,14 @@ class interpGrp {
 
   // return an interp name (value) for an interp id
   function name ($id) {
-    $val = $this->names[$id];
+    $val = $this->names["$id"];
     return $val;
   }
 
   // return which group an id belongs to
   function group ($id) {
     foreach ($this->groupnames as $g) {
-      if (in_array($id, $this->groups[$g])) {
+      if (in_array($id, $this->groups["$g"])) {
 	return $g;
       }
     }

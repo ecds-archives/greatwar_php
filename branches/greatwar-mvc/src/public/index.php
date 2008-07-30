ad8b6e9f1267d5a7e6af00379c95dd3639414c07
rsutton <rsutton@8ac14d8a-0c34-0410-b91d-f940ec8b8c98>
2008-07-30 15:49:02 +0000

    upgrade Xend_Layout to Zend_Layout; slightly different directory structure

--- branches/greatwar-mvc/src/public/index.php
+++ branches/greatwar-mvc/src/public/index.php
@@ -10,7 +10,8 @@ error_reporting(E_ALL|E_STRICT);
 ini_set("display_errors", "on");
 
 // ZendFramework
-ini_set("include_path", ini_get("include_path") . ":../lib:../app/:../app/modules/");
+ini_set("include_path", "../lib:../app/:../app/modules/:" . ini_get("include_path"));
+
 require("Zend/Loader.php");
 Zend_Loader::loadClass("Zend_Controller_Front");
 
@@ -23,8 +24,8 @@ $env_config	= new Zend_Config_Xml("../config/environment.xml", "environment");
 $config 	= new Zend_Config_Xml("../config/config.xml", $env_config->mode);
 
 //Create DB object
-$db = Zend_Db::factory($config->database->adapter, $config->database->params->toArray());
-Zend_Db_Table_Abstract::setDefaultAdapter($db);
+//$db = Zend_Db::factory($config->database->adapter, $config->database->params->toArray());
+//Zend_Db_Table_Abstract::setDefaultAdapter($db);
 
 //set default timezone
 date_default_timezone_set($config->timezone);
@@ -37,8 +38,10 @@ $front->setControllerDirectory(array("default" => "../app/controllers"));
 $front->addModuleDirectory("../app/modules");
 
 // Define Layout
-Xend_Layout::setup(array('path' => '../app/layouts',));
-Xend_Layout::setDefaultLayoutName('site');
+Zend_Layout::startMvc(array(
+    "layout" => "site",			// default layout
+    "layoutPath" => "../app/views/layouts",	// layout scripts directory
+    ));
 
 //set internal error handler
 $front->throwExceptions($env_config->display_exception);

14906715c807326a101ad492dbabdea999fcee21
ahickco <ahickco@8ac14d8a-0c34-0410-b91d-f940ec8b8c98>
2008-08-01 21:11:45 +0000

    modified credits page, added wwi.css, _banner.phtmlset up frontpage.phtml

--- branches/greatwar-mvc/src/app/controllers/IndexController.php
+++ branches/greatwar-mvc/src/app/controllers/IndexController.php
@@ -10,11 +10,15 @@ class IndexController extends Zend_Controller_Action {
      $this->view->assign("title", "Poetry and Postcards");
 
      // will use views/layouts/frontpage.phtml instead of site.phtml
-     //$this->_helper->layout->setLayout("frontpage");
+     $this->_helper->layout->setLayout("frontpage");
    }
 
   public function aboutAction() {
     $this->view->assign("title", "About this Site");
+  }
+
+  public function creditsAction() {
+    $this->view->assign("title", "Credits");
   }
 
    

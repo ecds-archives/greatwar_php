e3c9b2cf6d3a3d4b7a6897180407b5411e92156e
ahickco <ahickco@8ac14d8a-0c34-0410-b91d-f940ec8b8c98>
2010-11-19 22:29:22 +0000

    ingest script now adds LoC mappings.

--- greatwar-django/trunk/greatwar/postcards/management/commands/ingest_postcards.py
+++ greatwar-django/trunk/greatwar/postcards/management/commands/ingest_postcards.py
@@ -24,10 +24,14 @@ class Command(BaseCommand):
         make_option('--dry-run', '-n',
             dest='dry_run',
             action='store_true',
-            help='''Test the ingest, but don't actually do anything.'''),
+            help='''Test the ingest, but don\'t actually do anything.'''),
         )
 
+
+
+
     args = 'postcards.xml image_dir'
+    
 
     def handle(self, cards_fname, image_dir, dry_run=False, **options):
         verbosity = int(options['verbosity'])    # 1 = normal, 0 = minimal, 2 = all
@@ -38,6 +42,82 @@ class Command(BaseCommand):
         if not collection.exists:
             raise Exception(collection.pid + " is not in the repository. Do you need to syncrepo?")
 
+        def anas_simple(my_dict,a):
+            for ana in my_dict:
+                if ana in a:
+                    return my_dict[ana]
+
+
+#        def anas_complex(a,b):
+#            for ana in ana_lcc:
+#                if ana not in a:
+#                   return FALSE
+#                if ana in b:
+#                   return ana_lcc[ana] 
+    
+
+
+        #dictionary of lc subjects, simple (using 1 ana id) and complex (using 2 ana ids).
+        ana_lcs = {"nat-it":"World War, 1914-1918--Italy",
+                        "nat-fr":"World War, 1914-1918--France",
+                        "nat-us":"World War, 1914-1918--United States",
+                        "nat-de":"World War, 1914-1918--Germany",
+                        "nat-brit":"World War, 1914-1918--Great Britain",
+                        "nat-bel":"World War, 1914-1918--Belgium",
+                        "nat-au":"World War, 1914-1918--Austria",
+                        "nat-nl":"World War, 1914-1918--Netherlands",
+                        "nat-rus":"World War, 1914-1918--Russia",
+                        "nat-jp":" World War, 1914-1918--Japan",
+                        "nat-ee":"World War, 1914-1918--Eastern Europe",
+                        "nat-ca":"World War, 1914-1918--Canada", 
+                        "mil-nur":"Military Nursing",
+                        "con-h":"World War, 1914-1918--Humor", 
+                        "con-v":"World War, 1914-1918--Poetry", 
+                        "con-p":"World War, 1914-1918--Persons", 
+                        "con-m":"World War, 1914-1918--Memorials", 
+                        "con-r":"World War, 1914-1918--Destruction  and pillage",
+                        "con-f":"Flags in art",
+                        "con-el":"Uncle Elmer",
+                        "hf-p":"World War, 1914-1918--Propganda", 
+                        "hf-c":"World War, 1914-1918--Children", 
+                        "hf-w":"World War, 1914-1918--Women", 
+                        "hf-re":"World War, 1914-1918--Religious aspects", 
+                        "hf-ro":"World War, 1914-1918--Man-Woman relationships",
+                        }
+
+
+        ana_lcc_army = {"nat-fr":"France. Arm\xe9e", 
+                           "nat-brit":"Great Britain. Army", 
+                           "nat-bel":"Belgium. Arm\xe9e", 
+                           "nat-de":"Germany. Heer", 
+                           "nat-us":"United States. Army", 
+                           "nat-ca":"Canada. Canadian Army", 
+                           "nat-jp":"Japan. Rikugun", 
+                           "nat-au":"Austria. Arm\xe9e",
+                    }
+        ana_lcc_navy = {"nat-brit":"Royal Navy. Great Britain", 
+                           "nat-us":"United States. Navy", 
+                           "nat-fr":"France. Marine", 
+                           "nat-de":"Germany. Kriegsmarine", 
+                           "nat-ca":"Canada. Royal Canadian Navy",       
+                           }
+        #images use dc:type
+        ana_lcimage  = {"im-ph":"photograph",
+                        "im-pa":"painting",
+                        "im-dr":"drawing",
+                        "im-ca":"cartoon",
+                        "im-en":"engraving",
+                        "im-po":"poster",
+                        "im-s":"silk postcard", 
+                        }
+        #use dc:coverage
+        ana_lccoverage = {"t-wwi":"1914-1918",
+                          "t-pre":"Before 1914",
+                          "t-post":"After 1918",
+                          "t-ww2":"1939-1945",
+                          "t-post2":"After 1945",
+                          } 
+        
         # make a dictionary of subjects so type and value is easily accessible by id
         interps = collection.interp.content.interp_groups
         subjects = {}
@@ -74,16 +154,67 @@ class Command(BaseCommand):
             # TODO: handle postcards with text/poetry lines
 
             # convert interp text into dc: subjects
-            new_subjects = []
+            local_subjects = []
             for ana_id in c.ana.split():
+                #            ana_id = c.ana.split()
                 if ana_id in subjects:
-                    new_subjects.append('%s: %s' % subjects[ana_id])
+                    local_subjects.append('%s: %s' % subjects[ana_id])
                 else:
                     print 'ana id %s not recognized for %s' % (ana_id, c.entity)
-            obj.dc.content.subject_list.extend(new_subjects)
+            obj.dc.content.subject_list.extend(local_subjects)
 
+            lc_subjects = []
+            ana_ids = []
+            ana_ids = c.ana.split()
+            print 'DEBUG: %s are the ids for %s' % (ana_ids, c.entity) 
+            for ana_id in ana_ids:
+                if ana_id in ana_lcc_army:
+                    for ana_id2 in ana_ids:
+                        if  ana_id2 == "mil-a":
+                            ana_lc = ana_lcc_army[ana_id]
+                            lc_subjects.append('%s' % ana_lc)
+                            print '%s added to LC subjects list-army or navy' % ana_lc
+                            
+                if ana_id in ana_lcc_navy:
+                    for ana_id2 in ana_ids:
+                        if ana_id2 == "mil-na":
+                            ana_lc = ana_lcc_navy[ana_id]
+                            lc_subjects.append('%s' % ana_lc)
+                            print '%s added to LC subjects list-army or navy' % ana_lc
+                if ana_id in ana_lcs:
+                    ana_lc = anas_simple(ana_lcs, ana_id)
+                    lc_subjects.append('%s' % ana_lc)
+                    print '%s added to LC subjects list-nat, mil-nur, con, hf' % (ana_lc)
+#                else:         
+#                    print 'ana id %s not recognized for %s' % (ana_id, c.entity)
+            obj.dc.content.subject_list.extend(lc_subjects)
+
+            for ana_id in ana_ids:
+                my_dict = ana_lcimage
+                if ana_id in my_dict:
+                    print 'DEBUG %s found in image list' % ana_id
+                    ana_image = anas_simple(my_dict, ana_id)
+                    print 'DEBUG %s is the value for %s' % (ana_image, ana_id)
+                    lc_subjects.append('%s' % ana_image)
+                    print '%s added to LC subjects list-image type' % (ana_image)
+#                else:
+#                    print 'ana id %s not recognized for %s' % (ana_id, c.entity)
+            obj.dc.content.type_list.extend(lc_subjects)       
+
+            for ana_id in ana_ids:
+                my_dict = ana_lccoverage
+                if ana_id in my_dict:
+                    ana_cover = anas_simple(my_dict,ana_id)
+                    lc_subjects.append('%s' % ana_cover)
+                    print '%s added to LC subjects list-coverage' % ana_cover
+#                else:
+#                    print 'ana id %s not recognized for %s' % (ana_id, c.entity)
+            obj.dc.content.coverage_list.extend(lc_subjects)
+            
+                
             # common DC for all postcards
             obj.dc.content.type = 'image'
+            obj.dc.content.type = 'postcard'
             obj.dc.content.relation_list.extend(['The Great War 1914-1918',
                                  'http://beck.library.emory.edu/greatwar/'])
 

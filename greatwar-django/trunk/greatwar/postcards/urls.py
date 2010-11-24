26247aeab25c914acdf94acd884c7e8b6225caea
ahickco <ahickco@8ac14d8a-0c34-0410-b91d-f940ec8b8c98>
2010-11-24 20:52:55 +0000

    fixing broken tests

--- greatwar-django/trunk/greatwar/postcards/urls.py
+++ greatwar-django/trunk/greatwar/postcards/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('greatwar.postcards.views',
     url(r'^$', 'browse', name='browse'),
-    url(r'^(?P<subject>.*)$', 'browse', name='browse'),                    
     url(r'^about/$', 'summary', name='index'),    
+    url(r'^categories/(?P<subject>.*)$', 'browse', name='browse'),                    
     url(r'^(?P<pid>[^/]+)$', 'view_postcard', name='card'),
     url(r'^(?P<pid>[^/]+)/thumbnail/$', 'postcard_image', {'size': 'thumbnail'}, name='img-thumb'),
     url(r'^(?P<pid>[^/]+)/medium/$', 'postcard_image', {'size': 'medium'}, name='img-medium'),

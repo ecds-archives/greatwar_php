3f0adf6a5ad519648a1a9b74d6040bd79ccaf3be
athom09 <athom09@8ac14d8a-0c34-0410-b91d-f940ec8b8c98>
2011-04-13 16:46:08 +0000

    added util test that was removed from common.util.tests

--- greatwar-django/trunk/greatwar/postcards/tests.py
+++ greatwar-django/trunk/greatwar/postcards/tests.py
@@ -6,10 +6,12 @@ from os import path
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase as DjangoTestCase
+from django.conf import settings
 
 from eulcore.django.fedora.server import Repository
 
 from greatwar.postcards.fixtures.postcards import FedoraFixtures
+from util import get_pid_target, absolutize_url
 
 exist_fixture_path = path.join(path.dirname(path.abspath(__file__)), 'fixtures')
 exist_index_path = path.join(path.dirname(path.abspath(__file__)), '..', 'exist_index.xconf')
@@ -128,6 +130,32 @@ class PostcardViewsTestCase(DjangoTestCase):
         #               'Expected %s but returned %s for %s' % \
         #                (expected, response.status_code, thumb_url))
         # TODO: also test mimetype
+
+
+class UtilTest(DjangoTestCase):
+
+    #used to save real settings
+    _BASE_URL = None
+    _FEDORA_PIDSPACE = None
+
+    def setUp(self):
+        #save old values and assign test values
+        self.__BASE_URL = settings.BASE_URL
+        self._FEDORA_PIDSPACE = settings.FEDORA_PIDSPACE
+
+        settings.BASE_URL = 'http:/myurl.com'
+        settings.FEDORA_PIDSPACE = 'test-pidspace'
+
+
+    def tearDown(self):
+        settings.BASE_URL = self._BASE_URL
+        settings.FEDORA_PIDSPACE = self._FEDORA_PIDSPACE
+
+
+    def test_get_pid_target(self):
+        target = get_pid_target('postcards:card')
+        expected = '%s/postcards/%s:%s' %(settings.BASE_URL, settings.FEDORA_PIDSPACE, settings.PID_TOKEN)
+        self.assertEqual(target,expected)
         
 
         
